d86ce4bd5c837a72ace651b59e7f5a4ace5e44d1
ilyes-d <abdelkaderilyes.yahia@etu.usthb.dz>
2022-03-14 13:42:22 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Portfolio Template 
+
+### ToDo list 
+
+- [ ] Hero section 
+    - [ ] using `paritcle.js` as a background of my name 
+    - 
+    - Here will write my name and what is my passion 
+- [ ] About section 
+    -
+    - 
+
+
+
+
+my name in arabic ->
+يَحْيَ عَبْدُ القَادِرْ إلْيَاسْ
---
+++ js/particles_greeting.js
@@ -0,0 +1,102 @@
+const KeanuBold = new FontFace(
+  "keanu",
+  "url(./../assets/fonts/keanu/KeanuBold.ttf)"
+);
+KeanuBold.load().then(function (font) {
+  // with canvas we need to do this
+  document.fonts.add(font);
+  console.log("font loaded ");
+});
+const canvas = document.getElementById("canvas1");
+const context = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let particleArray = [];
+
+const mouse = {
+  x: null,
+  y: null,
+  radius: 150,
+};
+
+window.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+
+  // console.log(mouse.x, mouse.y);
+});
+
+context.fillStyle = "white";
+
+context.strokeStyle = "white";
+context.strokeRect(0, 0, 100, 100);
+context.font = "50px keanu"; //
+context.fillText("I", 0, 50);
+const data = context.getImageData(0, 0, 100, 100);
+
+class Particle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = 3;
+    this.baseX = this.x;
+    this.baseY = this.y;
+    this.density = Math.random() * 40 + 3;
+  }
+  draw() {
+    context.fillStyle = "white";
+    context.beginPath();
+    context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    context.closePath();
+    context.fill();
+  }
+  update() {
+    let dx = mouse.x - this.x;
+    let dy = mouse.y - this.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+    let forceDirectionX = dx / distance;
+    let forceDirectionY = dy / distance;
+    let maxDistance = mouse.radius;
+    let force = (maxDistance - distance) / maxDistance;
+    let directionX = forceDirectionX * force * this.density;
+    let directionY = forceDirectionY * force * this.density;
+    if (distance < mouse.radius) {
+      // this.size = 7;
+      this.x -= directionX;
+      this.y -= directionY;
+    } else {
+      if (this.x !== this.baseX) {
+        let dx = this.x - this.baseX;
+        this.x -= dx / 30;
+      }
+      if (this.y !== this.baseY) {
+        let dy = this.y - this.baseY;
+        this.y -= dy / 30;
+      }
+    }
+  }
+}
+function init() {
+  particleArray = [];
+  for (let i = 0; i < 500; i++) {
+    let x = Math.random() * canvas.width;
+    let y = Math.random() * canvas.height;
+    particleArray.push(new Particle(x, y));
+  }
+  // particleArray.push(new Particle(50 , 50));
+}
+
+init();
+console.log(particleArray);
+
+function animate() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].draw();
+    particleArray[i].update();
+  }
+  requestAnimationFrame(animate);
+}
+animate();
